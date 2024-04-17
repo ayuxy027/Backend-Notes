27dd57d1737267b84173438d59d70f95243d77cc
Ayush Yadav <ayush421301@gmail.com>
2024-04-17 17:24:23 +0530

    Added Notes and Testing File

--- 08RESTful APIs/index.js
+++ 08RESTful APIs/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const users = require("./testing.json");
 
 const app = express();
 
@@ -6,4 +7,40 @@ const PORT = 3000;
 
 //Routes
 
+// To get a list of users and display their names
+
+app.get('/users', (req, res) => {
+    const html = `<ul>
+    ${users.map(user => `<li>${user.first_name}</li>`).join("")}
+    </ul>`;
+    
+    res.send(html);
+});
+
+app.get('/api/users', (req, res) => {
+    return res.json(users);
+});
+
+// here we see at localhost:3000 and http://localhost:3000/api/users we can see the raw data
+
+// and at http://localhost:3000/user we can see the name of the user
+
+// this is called Hybrid Server 
+
+// Now for - GET /api/users/1 - get user with ID 1
+//           GET /api/users/2 - get user with ID 2
+
+// Here we can have unlimited number of users, so we have to make the ID Dynamic
+
+// This is called Dynamic Path Parameters
+
+// now we can make this by 
+
+// GET /api/users/:id
+
+// :id this is a variable now 
+
+
+
 app.listen(PORT, () => console.log(`Server listening on PORT: ${PORT}`));
+
